e7e9eda82f8a6dfa0ac107d716c7ca627eb62bb1
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-11-10 21:24:13 -0600

    API project 1.5 - making some different progress :S

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -5,25 +5,6 @@ class FlashcardsController < ApplicationController
   def index
     url = "http://kiei925.herokuapp.com/students.json?access_token=broncos"
     raw_data = open(url).read
-	parsed_data = JSON.parse(raw_data)
-    first = parsed_data[20]
-    @first_name = first["first_name"]
-    @last_name = first["last_name"]
-    @image_url = first["image_url"]
-    #results = parsed_data["results"]
-    
-    #geometry = first["geometry"]
-    #location = geometry["location"]
-    #@latitude = location["lat"]
-    #@longitude = location["lng"]
-    #@latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]]
-
-
-
-    parsed_data.each do |students|
-    @first_name = first["first_name"]
-    @last_name = first["last_name"]
-    @image_url = first["image_url"]
-    end
+	@parsed_data = JSON.parse(raw_data)
   end
-end
+ end
