115aeadd4f7d8324247dbeec8a271287e8ba469c
Bruce M. Klink <b-klink@kellogg.northwestern.edu>
2013-11-10 21:07:10 -0600

    API Project 1.5 - starting to work

--- app/controllers/flashcards_controller.rb
+++ app/controllers/flashcards_controller.rb
@@ -3,9 +3,27 @@ require 'json'
 
 class FlashcardsController < ApplicationController
   def index
-    # url = ?
-    # raw_data = ?
+    url = "http://kiei925.herokuapp.com/students.json?access_token=broncos"
+    raw_data = open(url).read
+	parsed_data = JSON.parse(raw_data)
+    first = parsed_data[20]
+    @first_name = first["first_name"]
+    @last_name = first["last_name"]
+    @image_url = first["image_url"]
+    #results = parsed_data["results"]
+    
+    #geometry = first["geometry"]
+    #location = geometry["location"]
+    #@latitude = location["lat"]
+    #@longitude = location["lng"]
+    #@latitude = parsed_data["results"][0]["geometry"]["location"]["lat"]]
 
-    # @students = ?
+
+
+    parsed_data.each do |students|
+    @first_name = first["first_name"]
+    @last_name = first["last_name"]
+    @image_url = first["image_url"]
+    end
   end
 end
